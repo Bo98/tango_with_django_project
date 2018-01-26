1440a86546534e662ee3a988f8c9fb48bef18a4e
Bo Anderson <2323889A@student.gla.ac.uk>
2018-01-26 20:26:25 +0000

    Chapter 10. I fixed a bug in it, hopefully the tests are ok.

--- rango/views.py
+++ rango/views.py
@@ -5,15 +5,36 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
+
+def visitor_cookie_handler(request):
+	visits = int(request.session.get("visits", "1"))
+	last_visit_cookie = request.session.get("last_visit", str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+	if (datetime.now() - last_visit_time).days > 0:
+		visits += 1
+		request.session["last_visit"] = str(datetime.now())
+	else:
+		request.session["last_visit"] = last_visit_cookie
+
+	request.session["visits"] = visits
 
 def index(request):
 	category_list = Category.objects.order_by("-likes")[:5]
 	page_list = Page.objects.order_by("-views")[:5]
 	context_dict = {"categories": category_list, "pages": page_list}
-	return render(request, "rango/index.html", context=context_dict)
+
+	visitor_cookie_handler(request)
+	context_dict["visits"] = request.session["visits"]
+
+	return render(request, "rango/index.html", context_dict)
 
 def about(request):
-	return render(request, "rango/about.html")
+	visitor_cookie_handler(request)
+	context_dict = {"visits": request.session["visits"]}
+
+	return render(request, "rango/about.html", context_dict)
 
 def show_category(request, category_name_slug):
 	context_dict = {}
